4b9e5f25dd3114ae85a557e81e2578d701dfbd01
rajiv ranjan pandey <raj.pandey914@gmail.com>
2024-03-14 23:22:41 +0530

    added ecommerce redirection

---
+++ src/components/ecommerce/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Ecommerce() {
+    window.location.href = "https://play.google.com/store/apps/details?id=co.fishshrimp";
+    return (
+        <></>
+    )
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { redirect, createBrowserRouter } from "react-router-dom";
 import Home from "../views/home/Home";
 import About from "../views/about/About";
 import OurVision from "../views/ourVision/OurVision";
@@ -8,7 +8,8 @@ import PrivacyPolicy from "../views/privacyPolicy/privacyPolicy";
 import TermsConditions from "../views/termsConditions/termsConditions";
 
 import MaiinWrapper from "../views/MaiinWrapper";
-
+import Ecommerce from "../components/ecommerce";
+// https://play.google.com/store/apps/details?id=co.fishshirip_vendor&pcampaignid=web_share
 
 const routes = createBrowserRouter([
     {
@@ -20,10 +21,12 @@ const routes = createBrowserRouter([
             { path: '/business-locations', element: <BusinessLocations /> },
             { path: '/our-vision', element: <OurVision /> },
             { path: '/about-us', element: <About /> },
+            { path: '/about-us_app', element: <About /> },
             { path: '/privacy-policy', element: <PrivacyPolicy /> },
             { path: '/privacy-policy_app', element: <PrivacyPolicy /> },
             { path: '/terms-conditions', element: <TermsConditions /> },
             { path: '/terms-conditions_app', element: <TermsConditions /> },
+            { path: '/ecommerce', element: <Ecommerce /> }
 
         ]
     },
